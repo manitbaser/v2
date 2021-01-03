2431065f51129d01cc04e11a58f4ff2895ab4380
manitbaser <manitb06@gmail.com>
2021-01-03 17:47:55 +0530

    Updates

---
+++ precache-manifest.c95311d23062be688818dbd7de4ee59b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a06d891d3aeccdc7e2aed442f32f454",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6453c7f32a2b79626bd",
+    "url": "/static/css/main.65c8efb8.chunk.css"
+  },
+  {
+    "revision": "04cf937c963fbbcab64f",
+    "url": "/static/js/2.cf89cfdb.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.cf89cfdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6453c7f32a2b79626bd",
+    "url": "/static/js/main.b51b1587.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "3a6b3fe3ee4161d958e6b81aaae432ec",
+    "url": "/static/media/manitbaser.3a6b3fe3.png"
+  },
+  {
+    "revision": "553abd6a90b49f72dcb1222b99ad33d5",
+    "url": "/static/media/resume.553abd6a.pdf"
+  }
+]);
