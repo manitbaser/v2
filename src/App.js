71856baf8566493ee299b0bc194d5ef97f51f0e8
manitbaser <manitb06@gmail.com>
2021-02-18 12:14:44 +0530

    Updated theme

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ const Home = React.forwardRef((props, ref) => {
       {/* <div className="bgstyle"> */}
       {/* <div style={{background: 'linear-gradient(65deg, #0d0a0b 0%, #000428 20%, #004e92 50%, #02aab0 80%, #00cdac 100%)',
           position:'center',}}> */}
-      <div style={{background: 'linear-gradient(80deg, #02aab0, #00cdac)'}}>
+      <div style={{background: 'linear-gradient(105deg, #029CF5, #15EDED, #15EDED, #029CF5)'}}>
       {about.show && (
         <AboutMe
           heading={about.heading}
