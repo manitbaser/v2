6bf467e81ebc119b5c663dba5050b3d16cddca0b
manitbaser <manitb06@gmail.com>
2021-04-23 14:35:16 +0530

    Updated Navbar and Resume button

--- src/components/home/AboutMe.jsx
+++ src/components/home/AboutMe.jsx
@@ -17,12 +17,11 @@ const AboutMe = ({ heading, message, link, imgSize, resume }) => {
   }, [link]);
 
   return (
-    <Jumbotron fluid id="aboutme" className="bg-transparent text-light min-vh-100 m-0">
-    <div className="jumbotron bg-transparent jumbotron-fluid m-0">
+    <Jumbotron fluid id="aboutme" className="bg-transparent text-light min-vh-100 m-0 d-flex align-items-center flex-wrap">
+    <div className="jumbotron bg-transparent min-vw-100 jumbotron-fluid m-0">
       <div className="container container-fluid">
         <div className="row">
-          <div className="col-5 d-none d-lg-block align-self-center">
-            {showPic && (
+          <div className="col-5 d-none d-lg-block align-self-center align-content-center">
               <div className="gradient_border_aboutme">
               <img
                 className="shadow-lg aboutme_img"
@@ -32,23 +31,22 @@ const AboutMe = ({ heading, message, link, imgSize, resume }) => {
                 height={imgSize}
               />
               </div>
-            )}
           </div>
 
-          <div className={`col-lg-${showPic ? "7" : "12"}`}>
+          <div className={`col-lg-${"7"}`}>
             <h2 className="display-4 mb-5 text-center">{heading}</h2>
             <p className="lead text-center">{message}</p>
             <br></br><br></br>
             {resume && (
               <p className="lead text-center">
                 <a
-                  className="btn btn-lg btn-outline-light"
+                  className="btn button_resume btn-lg"
                   href={resume}
                   target="_blank"
                   rel="noreferrer noopener"
                   role="button"
                   aria-label="Resume/CV"
-                  style={{color: '#00cdac'}}
+                  // style={{color: '#00cdac'}}
                 >
                   Resume
                 </a>
